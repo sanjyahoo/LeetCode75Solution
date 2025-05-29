7919dc79eb06d76e875775cc7d104685c54f0e00
sanjyahoo <sanjay.shar@yahoo.com>
2025-05-30 01:03:03 +0530

    new code

---
+++ src/PracticePackage/TopKFrequentElements.java
@@ -0,0 +1,51 @@
+package PracticePackage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TopKFrequentElements {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int x : nums) {
+            if (map.containsKey(x)) {
+                map.put(x, map.get(x) + 1);
+            } else {
+                map.put(x, 1);
+            }
+        }
+
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+
+        // Step 3: Fill buckets with numbers grouped by frequency
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int freq = entry.getValue();
+            int num = entry.getKey();
+            buckets[freq].add(num);
+        }
+
+        // Step 4: Collect top k frequent elements from buckets
+        int[] result = new int[k];
+        int count = 0;
+
+        for (int freq = buckets.length - 1; freq >= 0 && count < k; freq--) {
+            for (int num : buckets[freq]) {
+                result[count++] = num;
+                if (count == k) {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,3,3,3};
+        new TopKFrequentElements().topKFrequent(nums,2);
+    }
+}
