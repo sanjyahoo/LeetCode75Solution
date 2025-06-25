8d219654136c6affbcddf673bb6a64266368b47c
sanjyahoo <sanjay.shar@yahoo.com>
2025-06-26 01:00:41 +0530

    2

---
+++ src/PracticePackage/ReverseString.java
@@ -0,0 +1,22 @@
+package PracticePackage;
+
+public class ReverseString {
+    public void solveMe(char[] s){
+        char temp;
+        int left=0;
+        int right=s.length-1;
+
+        while(left<right){
+            temp = s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        new ReverseString().solveMe(s);
+    }
+}
